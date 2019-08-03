c3b292e70b99d71075c48ade5b603e6bf47cd0dc
nagarjundeepak <deepaknagarjun@gmail.com>
2019-08-03 22:00:41 +0530

    updated - server.js

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const mongoose = require ('mongoose');
 const app = express ();
 const port = 5000;
 
-app.get ('/api/users', (res, req) => {
+mongoose.connect (
+  'mongodb://nagarjun:oneplus5@ds157857.mlab.com:57857/mydatabase'
+);
+mongoose.connection.once ('open', () => {
+  console.log (`database is connected!`);
+});
+
+app.get ('/api/users', (req, res) => {
   const users = [{id: 1, name: 'nagarjun'}, {id: 2, name: 'ramesh'}];
-  res.json (users);
+  res.send (users);
 });
 
 app.listen (port, () => console.log (`server running on the port:${port}`));

fba04e79e2ea10cb9dcccb512a76e9b7ba2c46c4
nagarjundeepak <deepaknagarjun@gmail.com>
2019-08-04 11:17:05 +0530

    added - react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-import Home from './components/home';
+import Home from "./components/home";
 
-function App () {
-  return <div className="App"><Home /></div>;
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <switch>
+          <Route path="/" exact component={Home} />
+        </switch>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
